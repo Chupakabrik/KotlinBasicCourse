344fe018d6ac9d7eca5bb178c625b74fc73e5b79
Chupakabrik <chupakabrovich123@gmail.com>
2023-09-13 00:16:45 +0300

    KS-1-1 named variables

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_2.kt
@@ -0,0 +1,21 @@
+package Lesson_1
+
+fun main() {
+
+    var orderAmount: Int = 75
+    var thankfulText: String =  "Спасибо, что совершили покупку у нас!"
+
+    println(orderAmount)
+    println(thankfulText)
+
+    var numbersOfWorkers: Int = 2000
+
+//    println(numbersOfWorkers)
+
+    numbersOfWorkers = 1999
+
+    println(numbersOfWorkers)
+
+
+}
+
